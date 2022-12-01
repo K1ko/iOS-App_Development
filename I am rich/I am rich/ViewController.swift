c8fd5962ddc65147c9e3ec95dbb71f1cc22b0487
Kristián Červenka <kristian.cervenka@gmail.com>
2022-12-01 23:03:23 +0100

    :white_check_mark:

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Kristián Červenka on 01/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

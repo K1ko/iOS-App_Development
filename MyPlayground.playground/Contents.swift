c5d866dccfd0eb0317f80ac19aa4018864d38192
Kristián Červenka <kristian.cervenka@gmail.com>
2022-12-22 10:40:41 +0100

    :sparkles:

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,29 +1,12 @@
-import Foundation
+func exercise() {
 
-// Define a struct to represent a currency
-struct Currency {
-  let code: String
-  let name: String
-  let symbol: String
+    let numbers = [45, 73, 195, 53]
+    
+    //Write your code here
+    let computedNumbers = //Replace this comment with your code.
+    [numbers[0]*numbers[1],numbers[1]*numbers[2],numbers[2]*numbers[3],numbers[3]*numbers[0]]
+    
+    
+    print(computedNumbers)
+
 }
-
-// Define a dictionary of known currencies and their conversion rates
-let rates: [Currency: Double] = [
-  Currency(code: "USD", name: "United States Dollar", symbol: "$"): 1.0,
-  Currency(code: "EUR", name: "Euro", symbol: "€"): 1.18,
-  Currency(code: "GBP", name: "British Pound", symbol: "£"): 1.32
-]
-
-// Define a function to convert a given amount from one currency to another
-func convert(amount: Double, from fromCurrency: Currency, to toCurrency: Currency) -> Double {
-  guard let fromRate = rates[fromCurrency], let toRate = rates[toCurrency] else {
-    return 0.0
-  }
-  return (amount / fromRate) * toRate
-}
-
-// Test the converter with some sample input
-let amount = 100.0
-let fromCurrency = Currency(code: "USD", name: "United States Dollar", symbol: "$")
-let toCurrency = Currency(code: "EUR", name: "Euro", symbol: "€")
-print(convert(amount: amount, from: fromCurrency, to: toCurrency)) // should print 118.0

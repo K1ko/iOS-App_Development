6de20196b5f40e845e42ba9c710c2ddfcd22c230
Kristián Červenka <kristian.cervenka@gmail.com>
2022-12-24 08:28:37 +0100

    randomization

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,3 @@
-func exercise() {
+let randomNum = Int.random(in:1...100)
 
-    let numbers = [45, 73, 195, 53]
-    
-    //Write your code here
-    let computedNumbers = //Replace this comment with your code.
-    [numbers[0]*numbers[1],numbers[1]*numbers[2],numbers[2]*numbers[3],numbers[3]*numbers[0]]
-    
-    
-    print(computedNumbers)
-
-}
+print(randomNum)
